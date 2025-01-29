9a3dfd52ace972967f14441ab2afd036dd11daf3
AlexPrudius <prudius.av@ya.ru>
2025-01-29 09:54:55 +0500

    Test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Используем относительный путь
+  base: "/pioner-app/", // Указываем путь к репозиторию
 });
