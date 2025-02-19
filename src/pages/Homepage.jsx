74ee56ee771899a3bb451c4f4da10ea5868042da
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2025-02-19 07:01:07 +0500

    Background changed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,13 @@
 import EffectSection from "../components/Section/MapSection.jsx";
 import ProjectSection from "../components/Section/ProjectSection.jsx";
 import FilmSection from "../components/Section/FilmSection.jsx";
-import Background from "../components/Background.jsx";
 
 export default function HomePage() {
   return (
-    <div>
-      <div className="button-section">
-        <ProjectSection></ProjectSection>
-        <EffectSection></EffectSection>
-        <FilmSection></FilmSection>
-      </div>
-      <Background></Background>
+    <div className="button-section">
+      <ProjectSection></ProjectSection>
+      <EffectSection></EffectSection>
+      <FilmSection></FilmSection>
     </div>
   );
 }
