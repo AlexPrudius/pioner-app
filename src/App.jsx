06c046e6a3c7e65bcb15e94df7647f6ef24966f2
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2025-02-19 13:51:52 +0500

    Routing changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import "antd/dist/reset.css";
 import {
   Routes,
   Route,
-  Link,
   useLocation,
   useNavigate,
 } from "react-router-dom";
@@ -16,54 +15,20 @@ export default function App() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  // Если location.state.backgroundLocation отсутствует (например, при прямом переходе),
-  // подставляем фоновый контент с главной страницы.
-  const background = (location.state && location.state.backgroundLocation) || {
-    pathname: "/pioner-app",
-  };
-
   return (
     <main>
-      {/* Примеры ссылок для открытия модальных окон */}
-      {/* <header>
-        
-        <Link
-          to="/pioner-app/1"
-          state={{ backgroundLocation: location }}
-          className="link-button"
-        >
-          1 объект
-        </Link>
-        <Link
-          to="/pioner-app/2"
-          state={{ backgroundLocation: location }}
-          className="link-button"
-        >
-          2 объект
-        </Link>
-      </header> */}
-
-      {/* Фоновый контент: если пользователь перешёл напрямую по URL модалки,
-          мы подставляем фоновую страницу (HomePage) */}
-      <Routes location={background}>
-        <Route path="/pioner-app" element={<HomePage />} />
-        {/* Другие фоновые маршруты можно добавить здесь */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/:id"
+          element={
+            <ModalContent
+              open={true}
+              onClose={() => navigate("/")}
+            />
+          }
+        />
       </Routes>
-
-      {/* Динамическое модальное окно: рендерится, если путь не равен только "/pioner-app" */}
-      {location.pathname !== "/pioner-app" && (
-        <Routes>
-          <Route
-            path="/pioner-app/:id"
-            element={
-              <ModalContent
-                open={true}
-                onClose={() => navigate("/pioner-app")}
-              />
-            }
-          />
-        </Routes>
-      )}
     </main>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pioner-app/", // Указываем базовый путь для GitHub Pages
+  base: "/", // Указываем базовый путь для GitHub Pages
 });
 
 // export default defineConfig({
