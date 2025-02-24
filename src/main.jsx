78e47d5f6be22ec7cf392a5f2515f1a3f50151f1
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2025-02-24 13:18:55 +0500

    routing changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
