05272d93db94cfba766ccf3107c455d7bad6f6f8
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2025-02-24 15:45:47 +0500

    deploy changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename={process.env.NODE_ENV === 'production' ? '/pioner-app' : ''}>
+  <Router basename={process.env.NODE_ENV === 'production' ? '/pioner-app/' : '/'}>
     <App />
   </Router>
 );
